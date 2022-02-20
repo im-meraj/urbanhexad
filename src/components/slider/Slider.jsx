eab49a0d661721c81b4e3562cdece1069f17ca33
meraj-sheikh <meraj.sheikh.1997@gmail.com>
2022-02-20 21:53:22 +0530

    Fixed errors

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,6 +1,6 @@
 import './slider.css';
-import { motion } from 'framer-motion';
-import { useRef, useEffect, useState } from 'react';
+// import { motion } from 'framer-motion';
+// import { useRef, useEffect, useState } from 'react';
 
 const Slider = () => {
     return (
