cbb535125bd83972ae12355062599748b49c69c8
meraj-sheikh <meraj.sheikh.1997@gmail.com>
2022-02-20 21:51:21 +0530

    Removed yarn.lock file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from './components/navbar/Nav';
 import Banner from './components/hero/Banner';
 import Section1 from './components/section1/Section1';
 import Section2 from './components/section2/Section2';
+import Slider from './components/slider/Slider';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Banner />
       <Section1 />
       <Section2 />
+      <Slider />
     </>
   );
 }
---
+++ src/components/slider/Slider.jsx
@@ -0,0 +1,15 @@
+import './slider.css';
+import { motion } from 'framer-motion';
+import { useRef, useEffect, useState } from 'react';
+
+const Slider = () => {
+    return (
+        <div>
+            <div className="slider-container">
+                
+            </div>
+        </div>
+    );
+}
+
+export default Slider;
